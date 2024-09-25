a2d8821df0233f05445d84dce4c7d2c16fed96e3
MohamedFahim <MohamedFahim@99X-SL-DMFH4J3>
2024-09-25 23:57:27 +0530

    Implemented message service file

---
+++ src/app/shared/services/message.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.model';
+import { Message } from '../models/message.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+
+  private baseUrl = 'https://localhost:7195/api';
+
+  constructor(private http: HttpClient) { }
+
+  sendMessage(messageText: string, receiverId: number): Observable<number> {
+    const url = `${this.baseUrl}/message/send`;
+
+    const message = {
+      messageText: messageText
+    };
+
+    return this.http.post<number>(url, { message, receiverId });
+  }
+
+  getMessagesBetweenUsers(receiverId: number): Observable<Message[]> {
+    const url = `${this.baseUrl}/message/chat/${receiverId}`;  // Endpoint for getting messages
+
+    // Make the HTTP GET request to retrieve all messages
+    return this.http.get<Message[]>(url);
+  }
+  
+}
